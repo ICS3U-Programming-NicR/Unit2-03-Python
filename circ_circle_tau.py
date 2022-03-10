4d1b0d9f1e7e8fa02e6b08686ebbd595142a39e0
nictheriscer <nicolas.riscalas@stu.ocsb.ca>
2022-03-10 00:03:38 +0000

    fdas

--- circ_circle_tau.py
+++ circ_circle_tau.py
@@ -2,19 +2,18 @@
 
 # Created By: Nicolas Riscalas
 
-# Date: 02-28-2022
+# Date: 03-9-2022
 
 # Calculates the area and perimeter of a square
 
 
 import math
-
-TAU = math.pi * 2
+import constants
 
 
 def main():
     r = float(input("Enter the radius of a circle:\n"))
-    perimeter = TAU * r
+    perimeter = constants.TAU * r
     print("Perimeter of a circle = %.2f" % perimeter, "m")
 
 
---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created By: Nicolas Riscalas
+
+# Date: 03-9-2022
+
+# These are where the constants are
+import math
+
+TAU = math.pi * 2
